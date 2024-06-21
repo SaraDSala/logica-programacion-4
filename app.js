85f45eca1eded1e61f1d1d0b74f21bdb51f41b72
SaraSalazar <sara.salazar@genstudents.org>
2024-06-21 15:44:03 -0500

    Entrega logica programacion 4

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
 /* Elementos del HTML */
-const inputNumber = document.getElementById("numeroFactorial");
-const containerFactorial = document.getElementById("factorial");
+const inputNumber = document.getElementById("numeroSerie");
+const containerSerie = document.getElementById("serie");
 const contenedorRespuesta = document.querySelector(".contenedor-respuesta");
 
 const respuesta = document.createElement("h2");
-contenedorRespuesta.appendChild(respuesta)
+contenedorRespuesta.appendChild(respuesta);
 
-/* Función para calcular el factorial */
-const factorial = (numero) => {
-    if (numero === 0) {
-        return 1;
-    } else {
-        return numero * factorial(numero - 1);
+let sucesion = ''; // Variable para almacenar la serie
+
+/* Función para calcular la serie */
+const serie = (numero) => {
+    if (numero === 0||numero===1) {
+        return '0';
+    }
+    else {
+        sucesion = '0 1'; // Inicializar la serie con los primeros dos términos
+        
+        for (let contador = 2; contador < numero; contador++) {
+            let term = parseInt(sucesion.split(' ')[contador - 1]) + parseInt(sucesion.split(' ')[contador - 2]);
+            sucesion += ' ' + term;
+        }
+        
+        return sucesion;
     }
 };
 
 /* Función para validar el valor ingresado */
-const validarFactorial = () => {
-    let numeroToFactorial = inputNumber.value;
-    let numeroParseado = parseFloat(numeroToFactorial);
+const validarSerie = () => {
+    let numeroToSerie = inputNumber.value;
+    let numeroParseado = parseFloat(numeroToSerie);
 
     if (isNaN(numeroParseado)) {
-        containerFactorial.textContent = "Por favor ingrese un número válido";
+        containerSerie.textContent = "Por favor ingrese un número válido";
         console.error("Por favor ingrese un número válido");
     } else if (!Number.isInteger(numeroParseado)) {
-        containerFactorial.textContent = "Por favor ingrese un número entero, no se permiten decimales";
+        containerSerie.textContent = "Por favor ingrese un número entero, no se permiten decimales";
         console.error("Por favor ingrese un número entero, no se permiten decimales");
     } else {
-        let resultado = factorial(numeroParseado);
-        respuesta.textContent = `El factorial de ${numeroParseado} es:`
-        containerFactorial.textContent =  `${resultado}`;
-        console.log(`El factorial de ${numeroParseado} es: ${resultado}`);
+        let resultado = serie(numeroParseado);
+        respuesta.textContent = `Los primeros ${numeroParseado} términos de la serie:`
+        containerSerie.textContent =  `${resultado}`;
+        console.log(`${resultado}`);
     }
 };
 
 /* Añadir event listener para que cada vez que se ingrese un número, la función se actualice */
-inputNumber.addEventListener("input", validarFactorial);
+inputNumber.addEventListener("input", validarSerie);
 
 /* Inicialización */
-validarFactorial();
+validarSerie();
